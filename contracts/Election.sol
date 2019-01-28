61ea5bc872bbb8c51839413b3d2989bb5f67d51f
Sergei Stadnik <sergeisqa@gmail.com>
2019-01-28 12:23:55 -0700

    Truffle migrate  reset failed

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,16 +1,27 @@
 pragma solidity ^0.5.0;
 
 contract Election {
-
+//Model a candidate
 struct Candidate {
       uint id;
       string name;
       uint voteCount; 
 }
+//Store candidate
+mapping(uint=>Candidate) public candidates;
 
-constructor () public {
-candidate = "Candidate 1";
-      } 
+// Store Candidates Count
+uint public candidatesCount;
+
       
-}
+constructor () public {
+addCandidate("Candidate 1");
+addCandidate("Candidate 2");
+      }
+      function addCandidate(string _name ) private {
+            candidatesCount ++;
+            candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+      }
+
+      }
 
